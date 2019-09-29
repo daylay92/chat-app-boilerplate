3793cb6c04bd6a7d708759981de0514405c3b16c
Ayodele <daylay92@yahoo.com>
2019-09-29 03:05:13 +0100

    chore(updated): add logger

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 import 'dotenv/config';
 import { join } from 'path';
 import express from 'express';
-
+import morgan from 'morgan';
 
 const app = express();
 
 const publicPath = join(__dirname, '../public');
 
 app.use(express.static(publicPath));
-
+app.use(morgan('dev'));
 
 app.set('port', process.env.PORT || 300);
 
-app.listen(app.get('port'), ()=> {
-    console.log(`We are now live on port: ${app.get('port')}`)
-})
+app.listen(app.get('port'), () => {
+  console.log(`We are now live on port: ${app.get('port')}`);
+});
